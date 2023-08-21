208f91b08668da7a65ae22dd5a1f8923a8377f3d
Lucas Chagas <L.Candemil@edu.pucrs.br>
2023-08-20 23:30:35 -0300

    commit main

--- src/App.java
+++ src/App.java
@@ -1,11 +1,22 @@
 public class App {
-    public static void main(String[] args) {
-        Consultas consultas = new Consultas();
-        consultas.carregaDados();
+    public static void main(String[] args) {       
+        Repository repositorio = new Repository("poa_temps.txt");
+        Consultas consultas = new Consultas(repositorio);
+        
         System.out.println("Dia em que mais choveu no ano de 1980: ");
         System.out.println(consultas.diaQueMaisChoveuNoAno(1980));
         System.out.println("Datas em que choveu mais de 90 milimetros");
         consultas.datasEmQueChouveuMaisDe(90)
             .forEach(System.out::println);
+
+        System.out.println("Consulta de Temperatura maxima\n");
+        System.out.println(consultas.diasEmQue());
+        System.out.println();
+        
+        consultas.alteraConsultaPadrao(r -> r.getPrecipitacao() > 25);
+
+        System.out.println("Consulta de Precipatacao\n");
+        System.out.println(consultas.diasEmQue());
+
     }
 }
